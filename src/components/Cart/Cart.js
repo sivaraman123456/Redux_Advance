2d2b1f0a2098e3a33f46d83092a054674fe8fa07
MYPC\chepp <sivagithub123@gmail.com>
2025-10-17 11:13:24 +0300

    Addedntext

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,7 @@ import CartItem from './CartItem';
 const Cart = (props) => {
   return (
     <Card className={classes.cart}>
-      <h2>Your Shopping Cart</h2>
+      <h2>Your Shopping Cart Data setup</h2>
       <ul>
         <CartItem
           item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
